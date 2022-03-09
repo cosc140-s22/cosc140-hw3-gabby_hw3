97fe6ab030b443ad5d363b2c152e898dc62511e5
Gabby <65522279+BobbyTie@users.noreply.github.com>
2022-03-09 22:31:41 +0000

    finished part 1

--- main.py
+++ main.py
@@ -1,2 +1,2 @@
 import urlchecker
-import ghost
+# import ghost
--- urlchecker.py
+++ urlchecker.py
@@ -5,36 +5,75 @@
 #######################################################
 
 def urlchecker(url):
+  scheme, url = url.split("://")
+  
+  if not scheme == "http" and not scheme == "https":
+    return False
 
-    # your code should go here
+  
+  
+  pound_count = url.count("#")
+  question_count = url.count("?")
 
-    return True
+  if pound_count > 1 or question_count > 1:
+    return False
+  elif pound_count == 1 and question_count == 1:
+    if url.find("#") > url.find("?"):
+      return False
+
+  if url.count(" ") > 0:
+    return False
+
+  if url.count("/") == 0:
+    return False
+  
+  host, path = url.split("/", 1)
+
+  if path.count(":") > 0:
+    return False
+
+  if host.count(":") > 1:
+    return False
+  elif host.count(":") == 1:
+    hostname, port = host.split(":")
+    if not port.isdigit():
+      return False
+    if hostname == "":
+      return False
+  else:
+    if host == "":
+      return False
+  
+  return True
 
 
 def testurl():
-    urls = [ # valid
-      ['https://example.com/', True],
-      ['http://example.com/', True],
-      ['http://example.com/?query', True],
-      ['http://example.com/#fragment', True],
-      ['http://example/', True],
-      ['http://example/path/', True],
-      ['http://example/path', True],
-      ['https://example.com:3000/path#fragment?query', True],
-      ['https://example.com/path#fragment?query', True],
-      # invalid
-      ['htt://example/', False],
-      ['httpss://example/', False],
-      ['https://example/:3000', False],
-      ['https://example/?:3000?', False],
-      ['https://example/?:3000#', False],
-      ['https://example/xy z', False],
-      ['https://example/xyz:', False],
-      ['https://example', False],
-    ]
-    for url,expected in urls:
-        if urlchecker(url) != expected:
-            print(f"{url} is not valid, but your function claimed the opposite")
-        else:
-            print(f"{url} - ok")
+  urls = [ # valid
+    ['https://example.com/', True],
+    ['http://example.com/', True],
+    ['http://example.com/?query', True],
+    ['http://example.com/#fragment', True],
+    ['http://example/', True],
+    ['http://example/path/', True],
+    ['http://example/path', True],
+    ['https://example.com:3000/path#fragment?query', True],
+    ['https://example.com/path#fragment?query', True],
+    # invalid
+    ['htt://example/', False],
+    ['httpss://example/', False],
+    ['https://example/:3000', False],
+    ['https://example/?:3000?', False],
+    ['https://example/?:3000#', False],
+    ['https://example/xy z', False],
+    ['https://example/xyz:', False],
+    ['https://example', False],
+    ['https:///test', False]
+  ]
+  for url,expected in urls:
+    if urlchecker(url) != expected:
+      print(f"{url} is not valid, but your function claimed the opposite")
+    else:
+      print(f"{url} - ok")
 
+
+testurl()
